80b9015d708e6b1c8bf32eaeb0dd9d6ca61e3317
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2020-12-08 04:29:22 +0500

    added base app

---
+++ src/components/booklist/BookList.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Book from "../book/Book";
+
+const BookList = () => {
+  return (
+    <div>
+      <Book />
+    </div>
+  );
+};
+export default BookList;
